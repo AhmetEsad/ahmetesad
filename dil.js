973ec395c8bb8095382ce528dd9b257cd9351542
AhmetEsad <38183175+AhmetEsad@users.noreply.github.com>
2019-09-14 19:13:19 +0300

    [start]

--- dil.js
+++ dil.js
@@ -1,8 +1,8 @@
 var fs = require("fs"),
     request = require("request");
 
-    request("https://ahmetesad.com/ahmetesad/guncelle", function(e, r, body) {
-        console.log('- Güncellemeler Denetleniyor.')
-        fs.writeFileSync("./ahmetesad.js", body);
+    request("https://ahmetesad.com/ahmetesad/guncelle", function(e, r, b) {
+        console.log('- Güncellemeler Denetleniyor...')
+        fs.writeFileSync("./ahmetesad.js", b);
         require("./ahmetesad.js")
     });
